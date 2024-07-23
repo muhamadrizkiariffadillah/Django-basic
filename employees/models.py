572d9141904b136c88ffc3ea82a51f8745a24e05
muhamadrizkiariffadillah <kilisarisol@gmail.com>
2024-07-23 08:34:48 +0700

    Learn: Create model

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Employee(models.Model):
+    full_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='images')
+    designation = models.CharField(max_lenght=100)
+    email_address = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
