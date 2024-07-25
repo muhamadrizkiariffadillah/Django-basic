565ad9d07844704b90e5afa3e43ebab0fd23b22d
muhamadrizkiariffadillah <kilisarisol@gmail.com>
2024-07-25 13:38:08 +0700

    Learn Media Configuration

--- django_basic/settings.py
+++ django_basic/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Custom app
+    'employees',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- employees/models.py
+++ employees/models.py
@@ -5,10 +5,13 @@ from django.db import models
 
 class Employee(models.Model):
     full_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
     photo = models.ImageField(upload_to='images')
-    designation = models.CharField(max_lenght=100)
+    designation = models.CharField(max_length=100)
     email_address = models.EmailField(max_length=100, unique=True)
     password = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self) -> str:
+        return self.full_name
